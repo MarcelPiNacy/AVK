fef13757adf4aeeffd3530a62a78a11c54b809f7
MarcelPiNacy <marcelpi97@gmail.com>
2021-03-09 19:32:37 +0100

    Automated shader copy to build dir.

--- compile_shaders.bat
+++
@@ -1,7 +0,0 @@
-C:/VulkanSDK/1.1.121.2/Bin/glslc.exe avk/internal/shaders/bar_graph.vert -o avk/internal/shaders/bar_graph_vs.spv -O
-C:/VulkanSDK/1.1.121.2/Bin/glslc.exe avk/internal/shaders/bar_graph.frag -o avk/internal/shaders/bar_graph_fs.spv -O
-robocopy avk/internal/shaders/ build/Debug_x64/ *.spv /xo
-robocopy avk/internal/shaders/ build/Release_x64/ *.spv /xo
-robocopy avk/internal/shaders/ build/Debug_Win32/ *.spv /xo
-robocopy avk/internal/shaders/ build/Release_Win32/ *.spv /xo
-exit /B 0
--- pre_build_cmd/main.cpp
+++ pre_build_cmd/main.cpp
@@ -5,24 +5,25 @@
 
 
 
-const char* solution_dir;
-constexpr auto buffer_a_size = 1 << 12;
-char buffer_a[buffer_a_size];
+constexpr auto buffer_size = 1 << 12;
+char buffer_a[buffer_size];
+char buffer_b[buffer_size];
 
 
 
 int main(int argc, const char** args)
 {
-	if (argc != 2)
+	if (argc != 3)
 	{
 		puts("Missing solution directory!");
 		return -1;
 	}
 
-	solution_dir = args[1];
+	const char* solution_dir = args[1];
+	const char* configuration = args[2];
 	std::string resource;
 	std::string callbacks;
-	auto k = snprintf(buffer_a, buffer_a_size, "%savk\\internal\\windows-specific\\resource_template_a.txt", solution_dir);
+	auto k = snprintf(buffer_a, buffer_size, "%savk\\internal\\windows-specific\\resource_template_a.txt", solution_dir);
 	assert(k > 0);
 	FILE* file;
 	fopen_s(&file, buffer_a, "rb");
@@ -36,7 +37,7 @@ int main(int argc, const char** args)
 	std::string all_header =
 		"#pragma once\n#include \"../internal/main_array.h\"\n\n";
 	WIN32_FIND_DATAA data;
-	k = snprintf(buffer_a, buffer_a_size, "%savk\\algorithms\\*.cpp", solution_dir);
+	k = snprintf(buffer_a, buffer_size, "%savk\\algorithms\\*.cpp", solution_dir);
 	assert(k > 0);
 	auto finder = FindFirstFileA(buffer_a, &data);
 	assert(finder != INVALID_HANDLE_VALUE);
@@ -48,14 +49,14 @@ int main(int argc, const char** args)
 		auto i = std::find(data.cFileName, end, '.');
 		assert(i != end);
 		*i = '\0';
-		k = snprintf(buffer_a, buffer_a_size, "#define IDM_SORT_%s %llu\n", data.cFileName, counter);
+		k = snprintf(buffer_a, buffer_size, "#define IDM_SORT_%s %llu\n", data.cFileName, counter);
 		assert(k > 0);
 		resource.append(buffer_a);
 		++counter;
-		k = snprintf(buffer_a, buffer_a_size, "void %s(main_array array);\n", data.cFileName);
+		k = snprintf(buffer_a, buffer_size, "void %s(main_array array);\n", data.cFileName);
 		assert(k > 0);
 		all_header.append(buffer_a);
-		k = snprintf(buffer_a, buffer_a_size,
+		k = snprintf(buffer_a, buffer_size,
 			"case IDM_SORT_%s:\n"
 			"\talgorithm_thread::assign_body(%s);\n"
 			"\tbreak;\n", data.cFileName, data.cFileName);
@@ -63,13 +64,13 @@ int main(int argc, const char** args)
 		callbacks.append(buffer_a);
 	} while (FindNextFileA(finder, &data));
 	FindClose(finder);
-	k = snprintf(buffer_a, buffer_a_size, "%savk\\algorithms\\all.h", solution_dir);
+	k = snprintf(buffer_a, buffer_size, "%savk\\algorithms\\all.h", solution_dir);
 	assert(k > 0);
 	fopen_s(&file, buffer_a, "wb");
 	assert(file != nullptr);
 	fwrite(all_header.data(), 1, all_header.size(), file);
 	fclose(file);
-	k = snprintf(buffer_a, buffer_a_size, "%savk\\internal\\windows-specific\\resource_template_b.txt", solution_dir);
+	k = snprintf(buffer_a, buffer_size, "%savk\\internal\\windows-specific\\resource_template_b.txt", solution_dir);
 	assert(k > 0);
 	fopen_s(&file, buffer_a, "rb");
 	assert(file != nullptr);
@@ -80,17 +81,27 @@ int main(int argc, const char** args)
 	fseek(file, 0, SEEK_SET);
 	fread(resource.data() + prior_size, 1, file_size, file);
 	fclose(file);
-	k = snprintf(buffer_a, buffer_a_size, "%savk\\internal\\windows-specific\\resource.h", solution_dir);
+	k = snprintf(buffer_a, buffer_size, "%savk\\internal\\windows-specific\\resource.h", solution_dir);
 	assert(k > 0);
 	fopen_s(&file, buffer_a, "wb");
 	assert(file != nullptr);
 	fwrite(resource.data(), 1, resource.size(), file);
 	fclose(file);
-	k = snprintf(buffer_a, buffer_a_size, "%savk\\internal\\windows-specific\\sort_callbacks.inl", solution_dir);
+	k = snprintf(buffer_a, buffer_size, "%savk\\internal\\windows-specific\\sort_callbacks.inl", solution_dir);
 	assert(k > 0);
 	fopen_s(&file, buffer_a, "wb");
 	assert(file != nullptr);
 	fwrite(callbacks.data(), 1, callbacks.size(), file);
 	fclose(file);
+	k = snprintf(buffer_a, buffer_size, "%savk\\internal\\shaders\\bar_graph_fs.spv", solution_dir);
+	assert(k > 0);
+	k = snprintf(buffer_b, buffer_size, "%sbuild\\%s\\bar_graph_fs.spv", solution_dir, configuration);
+	assert(k > 0);
+	assert(CopyFileA(buffer_a, buffer_b, false));
+	k = snprintf(buffer_a, buffer_size, "%savk\\internal\\shaders\\bar_graph_vs.spv", solution_dir);
+	assert(k > 0);
+	k = snprintf(buffer_b, buffer_size, "%sbuild\\%s\\bar_graph_vs.spv", solution_dir, configuration);
+	assert(k > 0);
+	assert(CopyFileA(buffer_a, buffer_b, false));
 	return 0;
 }
